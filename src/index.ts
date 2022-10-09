739be62fd010731a92d01e105c231d51df547904
Ab_Kiros <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>
2022-10-09 22:23:26 +0200

    just adding express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
+import express  from "express";
 console.log("AboStore-BACKEND ---------#2022/10/09")
+
+const app = express();
+app.listen(3000,()=>{
+    console.log('The server is listening on PORT 3000')
+})
+
+app.get('/',(req,res)=>{
+    res.send({
+        "status":200,
+        "message":"This is just to test this endpoint is working!"
+    })
+})
