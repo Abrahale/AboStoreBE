03167285bb216f20e86023320ad0bea0527501fd
John Kiros <johnkiros31@gmail.com>
2022-12-10 12:07:49 +0200

    cart increment decrement quantity

--- src/routes/cartItem.ts
+++ src/routes/cartItem.ts
@@ -59,7 +59,7 @@ cartItemRouter.post("/", async (req: Request, res: Response) => {
        // console.log('current cart', cartInDb)
         if(cartInDb){
           let totals = getTotalItemsAndCount(cartInDb.cartItem)
-          result = await Cart.updateMany({id:cartInDb.id},{total:totals['total'],totalItems:totals['totalItems']})
+          result = await Cart.updateMany({id:cartInDb.id},{total:totals['total'],totalItems:totals['totalItems']}).exec();
         }
         else{
           return res.status(401).send({error:'No cart on session'});  
@@ -79,33 +79,42 @@ cartItemRouter.post("/", async (req: Request, res: Response) => {
     catch (error) {
       handleError(res,`Error adding cart item. Error: ${error}`);
     }
-    // let result;
-    // if(r){
-    //   cartReq.qty += r.qty;
-    //   result = await CartItem.findByIdAndUpdate(r.id,cartReq).exec();
-    //  // console.log(result)
-    //   const t = await Cart.findByIdAndUpdate(cartReq.cart,{$inc:{totalItems:result?.qty},$sum:{$multiply:[result?.qty,result?.populate({path:'product',select:'price'})]}});
-    //   console.log(t)
-    // }
-    // else{
-    //   result = await cartItem.save();
-    //   var itemInCart = await Cart.findOne({cartItem:result?.id}).exec();
-    //   if(itemInCart){
-    //     console.log('the item already exists in the cart')
-    //   }
-    //   else{
-    //     console.log('the item donesn\'t exist in db, and the cartItem is -->', result)
-    //     const cartR = await Cart.findByIdAndUpdate(cartReq.cart,{$push:{cartItem:result.id}}).exec();//Fix this so that it pushes only if it doesn't exist
-    //     console.log('Cart response after update', cartR)
-    //   }
-    // }
-   
-    //  handleResponse(res,result)
 } catch (error) {
   handleError(res,`Failed to create a new cart item. Error: ${error}`);
 }
 });
-
+cartItemRouter.post("/updateQty", async (req: Request, res: Response) => {
+  //PLEASE REFACTOR - this looks embaressing!!!
+  try{
+    const re = await CartItem.updateOne({id:req.body['cartItem_id'],product:req.body['product_id']},{$inc:{qty:req.body['inc']?+1:-1}}).exec();
+    const cartInDb = await Cart.findOne({id:req.body['cartId'], active:true,processed:false }).populate({path:'cartItem',select:'qty',populate:{path:'product',select:['price','quantity']}}).exec();
+    let totals = getTotalItemsAndCount(cartInDb.cartItem)
+    let a = await Cart.updateMany({id:cartInDb.id},{total:totals['total'],totalItems:totals['totalItems']}).exec();
+    const result = await Cart.findOne({id:req.body['cartId']}).populate({path:'cartItem',populate:{path:'product', populate:{path:'category',populate:{path:'department'}}}}).exec();
+    if (result) {
+      handleResponse(res,result)
+    }
+  }
+ catch(error){
+   handleError(res,`Failed to update cart-item. Error: ${error}`);
+ }
+});
+cartItemRouter.post("/delete", async (req: Request, res: Response) => {
+  //Need to implement this!!!
+  try{
+    const re = await CartItem.updateOne({id:req.body['cartItem_id'],product:req.body['product_id']},{$inc:{qty:req.body['inc']?+1:-1}}).exec();
+    const cartInDb = await Cart.findOne({id:req.body['cartId'], active:true,processed:false }).populate({path:'cartItem',select:'qty',populate:{path:'product',select:['price','quantity']}}).exec();
+    let totals = getTotalItemsAndCount(cartInDb.cartItem)
+    let a = await Cart.updateMany({id:cartInDb.id},{total:totals['total'],totalItems:totals['totalItems']}).exec();
+    const result = await Cart.findOne({id:req.body['cartId']}).populate({path:'cartItem',populate:{path:'product', populate:{path:'category',populate:{path:'department'}}}}).exec();
+    if (result) {
+      handleResponse(res,result)
+    }
+  }
+ catch(error){
+   handleError(res,`Failed to update cart-item. Error: ${error}`);
+ }
+});
 cartItemRouter.post("/update", async (req: Request, res: Response) => {
   const id = req.body.id
   try {
