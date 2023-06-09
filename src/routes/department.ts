cf474be3502e8a7621acf22417f06b0f0fddfe17
Abrahale Kiros <abrahalekiros@gmail.com>
2023-06-09 12:10:13 +0200

    deleteing department

--- src/routes/department.ts
+++ src/routes/department.ts
@@ -1,7 +1,11 @@
 import express, { Request, Response } from "express";
 import { handleResponse, handleError } from "../middleware/response.middeware";
 import { Department, IDepartment } from "../models/department";
+import  asyncHandler  from 'express-async-handler';
+import { isValidObjectId } from "../utils/validateObjectId.utils";
 export const departmentRouter = express.Router();
+
+const ash = asyncHandler
 departmentRouter.use(express.json());
 // GET
 departmentRouter.get("/", async (_req: Request, res: Response) => {
@@ -36,3 +40,22 @@ departmentRouter.post("/", async (req: Request, res: Response) => {
   handleError(res,`Failed to create a new department. Error: ${error}`);
 }
 });
+
+departmentRouter.get("/delete/:",ash( async (req: Request, res: Response) => {
+  const id = req.query.id;
+  isValidObjectId(id)
+      const result =  await Department.findByIdAndDelete({_id:id});
+      if (result != null) {
+        try {
+          const departments = await Department.find({}).exec();
+          let responseObject = {message:"Department deleted successfully",departments:[...departments]}
+          handleResponse(res,responseObject)
+        } catch (error: any) {
+            handleError(res,{message:"Department deleted successfully, but there was an error getting list of departments",error:{...error}})
+        }
+      }
+      else{
+        throw new  Error(`Departments Not Found with id: ${id}`);
+      }
+  }
+));
