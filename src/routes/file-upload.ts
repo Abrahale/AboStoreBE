9355eb0af908e7d61cc888bc970326c98522194a
Abrahale Kiros <abrahalekiros@gmail.com>
2023-06-22 14:56:00 +0200

    getting env variables

--- src/routes/file-upload.ts
+++ src/routes/file-upload.ts
@@ -1,10 +1,9 @@
 import express, { Request, Response } from "express";
 import {  s3, uploadImage, deleteFile } from "../middleware/file-upload.middleware";
-import { ListObjectsV2Command } from '@aws-sdk/client-s3';
-import { S3Client } from '@aws-sdk/client-s3';
 import { listS3Files } from "../helpers/functions";
 import { handleError, handleResponse } from "../middleware/response.middeware";
-
+import dotenv from 'dotenv';
+dotenv.config();
 export const fileUploadRouter = express.Router()
 
 fileUploadRouter.post('/upload',async(req:Request,res:Response) => {
